e8f5fa6def80325b2dc1848fadac9c0f38d6ea1e
Oliver Young <ocam12@btinternet.com>
2025-08-01 22:56:52 +0100

    Added section 4 + 5 nd started footer

--- uob/script.js
+++ uob/script.js
@@ -32,4 +32,19 @@ function updateDots(index) {
 
 setInterval(() => {
     goToSlide((currentSlide + 1) % slides.length);
-}, 7500);
+}, 5000);
+
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+      }
+    });
+  }, {
+    threshold: 0.6 // Trigger when 10% of the element is visible
+  });
+
+  document.querySelectorAll('.slide-in-element').forEach(el => {
+    observer.observe(el);
+  });
